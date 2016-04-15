65fcedef2a85f9747c8697532715855d09307f24
Nick Stalter <nickstalter@gmail.com>
2016-04-15 14:41:09 -0400

    removed unused authenticate function, since we use a plug now

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -29,17 +29,6 @@ defmodule Discovery.UserController do
     end
   end
 
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access this page")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     user = Repo.get!(User, id)
     render(conn, "show.html", user: user)
