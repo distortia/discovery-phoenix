aa596ca0e75b88eb6c31589c0025bec4b883b476
dpappa <darrell.pappa@pointstart.us>
2016-04-23 15:11:27 -0400

    working email link

--- lib/discovery/mailer.ex
+++ lib/discovery/mailer.ex
@@ -6,11 +6,11 @@ end
 defmodule Discovery.Email do
   import Bamboo.Email
 
-  def welcome_email do
+  def welcome_email(company, user) do
     new_email
-    |> to("me@darrellpappa.com")
+    |> to(user)
     |> from("support@alphaity.io")
     |> subject("Discovery - Invitation")
-    |> html_body("You have been invited to join \"Company_Name\" in Discovery. Click this link to join!")
+    |> html_body("You have been invited to join #{company.name}in Discovery. Click this link to join!")
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,17 +2,22 @@ defmodule Discovery.PageController do
   use Discovery.Web, :controller
   alias Discovery.Email
   alias Discovery.Mailer
+  alias Discovery.Company
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def email(conn, %{"company" => company}) do
-  	Email.welcome_email()
+  	company = 
+  	Repo.get!(Company, company)
+  	|> IO.inspect()
+  	user="me@darrellpappa.com"
+  	Email.welcome_email(company, user)
   	|> Mailer.deliver_later()
 
   	conn
   	|> put_flash(:info, "Invitiation sent!")
-  	|> redirect(to: company_path(conn, :show, company))
+  	|> redirect(to: company_path(conn, :show, company.id))
   end
 end
