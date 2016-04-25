e81bde4508a0d53fafc7967e2b51d8a0eb4fa1be
neilnagarsheth <neilnagarsheth@gmail.com>
2016-04-24 23:11:15 -0400

    creating a plug for self and admin access

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,6 +4,7 @@ defmodule Discovery.UserController do
   alias Discovery.User
 
   plug :authenticate_user when action in [:index, :show]
+  plug :access_user when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
     users = 
@@ -55,92 +56,61 @@ defmodule Discovery.UserController do
   end
 
   def edit(conn, %{"id" => id}) do
-    cond do
-      self_access_only(conn, id) == {:ok, conn} or admin_access_only(conn, id) == {:ok, conn} ->
-        user = Repo.get!(User, id)
-        changeset = User.changeset(user)
-        currentUser = Repo.get!(User, conn.assigns.current_user.id)
-        render(conn, "edit.html", user: user, changeset: changeset, currentUser: currentUser)
-      true ->
-        conn
-          |> put_flash(:error, "Error, You do not have access this page")
-          |> redirect(to: user_path(conn, :index))
-          |> halt()
-    end
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user)
+    currentUser = Repo.get!(User, conn.assigns.current_user.id)
+    render(conn, "edit.html", user: user, changeset: changeset, currentUser: currentUser)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
-    cond do
-      self_access_only(conn, id) == {:ok, conn} or admin_access_only(conn, id) == {:ok, conn} ->
-        user = Repo.get!(User, id)
-        # If the user is going to assign a new owner, change their current role to an admin
-        if user_params["role"] == "Owner" do
-          if change_owner(conn) == {:error, conn} do
-            conn
-             |> put_flash(:error, "Error Updating User")
-             |> redirect(to: user_path(conn, :index))
-             |> halt()
-          end
-        end
-        changeset = User.update_changeset(user, user_params)
-        case Repo.update(changeset) do
-          {:ok, user} ->
-            conn
-            |> put_flash(:info, "User updated successfully.")
-            |> redirect(to: user_path(conn, :show, user))
-          {:error, changeset} ->
-            render(conn, "edit.html", user: user, changeset: changeset)
-        end
-      true ->
+    user = Repo.get!(User, id)
+    # If the user is going to assign a new owner, change their current role to an admin
+    if user_params["role"] == "Owner" do
+      if change_owner(conn) == {:error, conn} do
         conn
-          |> put_flash(:error, "Error, You do not have access this page")
-          |> redirect(to: user_path(conn, :index))
-          |> halt()
+         |> put_flash(:error, "Error Updating User")
+         |> redirect(to: user_path(conn, :index))
+         |> halt()
+      end
+    end
+    changeset = User.update_changeset(user, user_params)
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User updated successfully.")
+        |> redirect(to: user_path(conn, :show, user))
+      {:error, changeset} ->
+        render(conn, "edit.html", user: user, changeset: changeset)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    cond do
-      self_access_only(conn, id) == {:ok, conn} or admin_access_only(conn, id) == {:ok, conn} ->
-        user = Repo.get!(User, id)
-        tickets = Repo.all(assoc(user, :tickets))
-        case length(tickets) do
-          0 ->
-            Repo.delete!(user)
-            conn
-            |> put_flash(:info, "User deleted successfully.")
-            |> redirect(to: user_path(conn, :index))
-          _ ->
-            conn
-             |> put_flash(:error, "Please re-assign all tickets from this user before deleting")
-             |> redirect(to: user_path(conn, :index))
-             |> halt()
-        end
-      true ->
+    user = Repo.get!(User, id)
+    tickets = Repo.all(assoc(user, :tickets))
+    case length(tickets) do
+      0 ->
+        Repo.delete!(user)
         conn
-         |> put_flash(:error, "Error, You do not have access to delete")
+        |> put_flash(:info, "User deleted successfully.")
+        |> redirect(to: user_path(conn, :index))
+      _ ->
+        conn
+         |> put_flash(:error, "Please re-assign all tickets from this user before deleting")
          |> redirect(to: user_path(conn, :index))
          |> halt()
     end
   end
 
-  # This only allows the specific user access to updated/delete their profile
-  def self_access_only(conn, id) do
-    # Compare the current user to the id from the path
-    if to_string(conn.assigns.current_user.id) == id do
-      {:ok, conn}
+  #Plug used to determine if the user has access to do an action within the User model space
+  def access_user(conn, _options) do
+    otherUser = Repo.get!(User, conn.params["id"])
+    if to_string(conn.assigns.current_user.id) == conn.params["id"] or conn.assigns.current_user.role == "Owner" or (conn.assigns.current_user.role == "Admin" and otherUser.role != "Owner")  do
+      conn
     else
-      {:error, conn}
-    end
-  end
-
-  # This checks if the user is an admin or an owner
-  def admin_access_only(conn, id) do
-    user = Repo.get!(User, conn.assigns.current_user.id)
-    otherUser = Repo.get!(User, id)
-    cond do
-      (user.role == "Admin" and otherUser.role != "Owner") or user.role == "Owner" -> {:ok, conn}
-      true -> {:error, conn}
+       conn
+         |> put_flash(:error, "Error, You do not have access do this")
+         |> redirect(to: user_path(conn, :index))
+         |> halt()   
     end
   end
 
