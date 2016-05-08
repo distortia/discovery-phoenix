c03d6ee33d263c26cd549bfb55373d74267755c3
Nick Stalter <nickstalter@gmail.com>
2016-05-07 23:01:26 -0400

    Added stubs for auth tests Renamed company in company tests to a cooler company name

--- test/controllers/auth_test.exs
+++ test/controllers/auth_test.exs
@@ -3,21 +3,57 @@ defmodule Discovery.AuthTest do
 
   alias Discovery.Auth 
 
+  setup %{conn: conn} = config do
+    if email = config[:login_as] do
+      user = insert_user(email: email)
+      conn = assign(conn, :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
   test "Login with valid user", %{conn: conn} do
-    assert false
-    # assert get_flash(conn, :info) == "Welcome back!"
-    # assert html_response(conn, 200) =~ "Listing tickets"
+    user = insert_user(%{first_name: "Test", last_name: "User", email: "testuser@test.com", password: "Val1dP@ass", role: "User", auth_id: "123"})
+    assert login(conn, user)
+  end
+
+  test "Login by email and pass", %{conn: conn} do
+    
+  end
+
+  test "Login by email and pass with invalid password" do
+    
+  end
+
+  test "Login by email and pass with no user or password" do
+    
   end
 
   test "login with invalid email", %{conn: conn} do
-    assert false
+  
   end
 
   test "login with invalid password", %{conn: conn} do
-    assert false
+  
   end
 
-  test "logout", %{conn: conn} do
-    assert false
+  @tags login_as: "unittest@unittest.com"
+  test "logout", %{conn: conn, user: user} do
+    assert logout(conn)
+  end
+
+  test "Authenticate user" do
+    
   end 
+
+  @tags login_as: "unittest@unittest.com"
+  test "Authenticated not allowed - while logged in" do
+    
+  end  
+
+  @tags login_as: "unittest@unittest.com"
+  test "Authenticated not allowed - while not logged in" do
+    
+  end
 end
